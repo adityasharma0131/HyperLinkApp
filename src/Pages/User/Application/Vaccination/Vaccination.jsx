d86502d20c2c4e322327a871f76896a5b4e39943
Aditya Suresh Sharma <adityasharma0431@gmail.com>
2025-07-26 15:36:47 +0530

    Benifits, Report Bannner Completed

--- src/Pages/User/Application/Vaccination/Vaccination.jsx
+++ src/Pages/User/Application/Vaccination/Vaccination.jsx
@@ -10,6 +10,11 @@ import ChildVacc from "../../../../assets/catchildhoodvaccines.svg";
 import AdultVacc from "../../../../assets/catadultvaccines.svg";
 import TravelVacc from "../../../../assets/cattravelvaccines.svg";
 import PrescriptionBG from "../../../../assets/PrescriptionBG.svg";
+import HealthRecord from "../../../../assets/healthrecord.png";
+
+import { FaHome } from "react-icons/fa";
+import { FaRibbon } from "react-icons/fa6";
+import { FaUserCheck } from "react-icons/fa";
 
 const Vaccination = () => {
   return (
@@ -80,6 +85,38 @@ const Vaccination = () => {
           <div className="prescription-image">
             <img src={PrescriptionBG} alt="Upload Illustration" />
           </div>
+        </div>
+      </div>
+
+      <div className="vaccine-benefits">
+        <div className="benefit-card">
+          <FaHome className="benefit-icon" />
+          <p>Home vaccination support with certified nurses</p>
+        </div>
+        <div className="benefit-card">
+          <FaRibbon className="benefit-icon" />
+          <p>Special focus on cancer prevention & women’s wellness</p>
+        </div>
+        <div className="benefit-card">
+          <FaUserCheck className="benefit-icon" />
+          <p>Personalized vaccine suggestions based on health profile</p>
+        </div>
+      </div>
+
+      <div className="report-banner">
+        <div className="report-text">
+          <h2>
+            <span className="highlight">YOUR REPORT</span> <br />
+            <span className="highlight">IS SECURE</span>
+          </h2>
+          <p>
+            Your results and insights have been saved in your Locker. Access
+            anytime from your Profile tab.
+          </p>
+          <AppButton text="View Records" />
+        </div>
+        <div className="report-image">
+          <img src={HealthRecord} alt="Health Record Secure" />
         </div>
       </div>
     </div>
